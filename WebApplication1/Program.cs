16a55b8a80998ebac4bd6396946c501edd017691
Christian Muley <christian.muley@vdabcampus.be>
2026-03-19 13:56:27 +0100

    Structure Fix

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Serve index.html by default
+app.UseDefaultFiles();
+
+// Serve files from wwwroot (html, css, js, images, json, etc.)
+app.UseStaticFiles();
 
 app.Run();
